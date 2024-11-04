8e2e97434a7b55bce4e134d5e2d470d5e9287531
Arjun Manjunath <arjunmanjunath80@gmail.com>
2024-11-04 23:28:00 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,22 @@
+import os
 from flask import Flask
+from api.routes import appbp
+from api.models import db
+import pymysql
+from dotenv import load_dotenv
+
+load_dotenv()
+pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.urandom(24)
+db.init_app(app)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+try:
+    with app.app_context():
+        app.register_blueprint(appbp)
+        db.create_all()
+except Exception as e:
+    print('db.create_all() errored out')
